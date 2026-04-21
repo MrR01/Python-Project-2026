ba7e160208e17612683efc3db44d48af410da452
MrR01 <42345426+MrR01@users.noreply.github.com>
2026-04-21 17:10:00 +0100

    Update Model_Pipeline.py

--- Model_Pipeline.py
+++ Model_Pipeline.py
@@ -18,20 +18,20 @@ from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 
-//2. Load Data
+#2. Load Data
 df = pd.read_csv("adult.csv")
 
 print(df.head())
 print(df.info())
 
-//3. Basic Cleaning
+#3. Basic Cleaning
 # Replace '?' with NaN (common in Adult dataset)
 df.replace("?", np.nan, inplace=True)
 
 # Target variable
 df['Income'] = df['Income'].apply(lambda x: 1 if '>50K' in x else 0)
 
-4. Train/Test Split (BEFORE preprocessing)
+#4. Train/Test Split (BEFORE preprocessing)
 X = df.drop("Income", axis=1)
 y = df["Income"]
 
@@ -39,7 +39,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-5. Feature Engineering
+#5. Feature Engineering
 numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
 categorical_features = X.select_dtypes(include=['object']).columns
 
@@ -55,37 +55,37 @@ categorical_pipeline = Pipeline([
 ])
 
 
-// Combine into one pipeline:
+# Combine into one pipeline:
 preprocessor = ColumnTransformer([
     ("num", numeric_pipeline, numeric_features),
     ("cat", categorical_pipeline, categorical_features)
 ])
 
-6. Full Model Pipeline
+#6. Full Model Pipeline
 model = Pipeline([
     ("preprocessing", preprocessor),
     ("classifier", RandomForestClassifier(n_estimators=200, random_state=42))
 ])
 
-// 7. Cross Validatio
+#7. Cross Validatio
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 scores = cross_val_score(model, X_train, y_train, cv=cv, scoring="roc_auc")
 
 print("ROC AUC:", scores.mean())
 
-// 8. Train Final Model
+#8. Train Final Model
 model.fit(X_train, y_train)
 
 
-//9. Evaluatation
+#9. Evaluatation
 y_pred = model.predict(X_test)
 y_prob = model.predict_proba(X_test)[:, 1]
 
 print(classification_report(y_test, y_pred))
 print("ROC AUC:", roc_auc_score(y_test, y_prob))
 
-//10. Confusion Matrix
+#10. Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
 
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
